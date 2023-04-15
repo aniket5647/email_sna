bdb27ddbd051a033f6f3d7b830c4b0529090b7d8
aniket5647 <128222681+aniket5647@users.noreply.github.com>
2023-04-15 14:18:11 +0530

    Update dep1.py

--- dep1.py
+++ dep1.py
@@ -1,36 +1,23 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
-with open('./dep1.csv', 'r') as f:
+with open('./all_dept.csv', 'r') as f:
     data_file = f.readlines()
 
 # Create graph object
 Graph1 = nx.DiGraph()
+matrix = [[0 for _ in range(2000)] for _ in range(2000)]
 
 # Add nodes and edges to graph
 for line in data_file:
     source, dest, time = line.strip().split(' ')
+    matrix[int(source)][int(dest)] +=1 
     Graph1.add_edge(int(source), int(dest), weight=float(time))
     
-node_list = sorted(Graph1.nodes())
-adj_matrix = np.zeros((len(node_list), len(node_list)))
+# print(matrix)
 
-for source, dest, weight in Graph1.edges.data('weight'):
-    adj_matrix[node_list.index(source), node_list.index(dest)] = weight
-
-# Print adjacency matrix
-print(adj_matrix)
-
-# nx.draw(Graph1, with_labels=True, font_size=5)
-# plt.title("Department-1 Visualization")
-# # plt.bar_label("Department-1 Visualization")
-# plt.show()
-# Define node colors
-#'5': ['2', '13', '19', '27', '29', '209', '226'], 
-node_color = ['blue' if node != '172' else 'red' for node in Graph1.nodes()]
 # Draw graph
 pos = nx.spring_layout(Graph1)  # Compute layout
-nx.draw_networkx_nodes(Graph1, pos, node_color=node_color)
 nx.draw_networkx_edges(Graph1, pos)
 nx.draw_networkx_labels(Graph1, pos, font_size=5)
 
@@ -43,13 +30,18 @@ out_degrees = dict(Graph1.out_degree())
 in__degrees_list = {}
 for node in Graph1.nodes():
     in__degrees_list[node] = in_degrees[node]
+    
 print("Indegree:")
 # print(in__degrees_list)
+
+
 out__degrees_list = {}
 for node in Graph1.nodes():
     out__degrees_list[node] = out_degrees[node]
+    
 print("Outdegree:")
 # print(out__degrees_list)
+
 degree_centrality = nx.degree_centrality(Graph1)
 total_centrality = sum(degree_centrality.values())
 print("Degree Centrality:")
@@ -80,19 +72,19 @@ for user in Graph1.nodes():
 
 # Print total contribution for each user
 print("Total Contribution")
-# print(total_contributio
+# print(total_contributio)
+
+
 #Now find the social score 
-degree_centrality_weight = 0.5
+# degree_centrality_weight = 0.5
 social_scores = {}
 for user in Graph1.nodes():
     contribution = total_contribution[user]
     # response = response_scores[user]
     degree = degree_centrality[user]
     avgCentralitty_array = avg_centralitty_array[user]
-    social_score = contribution / (avgCentralitty_array*(in_degrees[node] + out_degrees[node]))
+    social_score = contribution/ (avgCentralitty_array*(in_degrees[node] + out_degrees[node]))
     social_scores[user] = social_score
-    # social_score[node] = (in_degrees[node] + out_degrees[node]) * avg_centralitty_array[node] * response_score[node]
-    
 
 # Print social score for each user
 print("Social Scores")
@@ -106,10 +98,10 @@ for node, social_score in social_scores.items():
     avg_social_score[node] = (social_score / total_social_score)*100
     
 print("AVG Social/ Centrality")
-# print(avg_social_score)
+print(avg_social_score)
+
 
 neighbors_list = {}
-
 # Loop through each node in the graph
 for node in Graph1.nodes():
     neighbors = list(Graph1.neighbors(node))
@@ -121,57 +113,24 @@ print(neighbors_list)
 
 
 
-# degree_between_neighbors = {}
-# for node, neighbors in Graph1.adjacency():
-#     for neighbor in neighbors:
-
-#         # get the degree of the node and neighbor
-#         node_degree = Graph1.degree(node)
-#         neighbor_degree = Graph1.degree(neighbor)
-
-#         # get the in-degree and out-degree of the neighbor
-#         in_degree = Graph1.in_degree(neighbor)
-#         out_degree = Graph1.out_degree(neighbor)
-
-#         # add the node, neighbor, node degree, neighbor degree, in-degree, and out-degree to the dictionary
-#         degree_between_neighbors[(node, neighbor)] = [node_degree, neighbor_degree, in_degree, out_degree]
-
-# # print the degree_between_neighbors dictionary
-# print("degree_between_neighbors")
-# print(degree_between_neighbors)
-degree_dict = {}
-
-# Loop over each node in the graph
-for node in Graph1.nodes():
-    # Initialize the in-degree and out-degree of the node to zero
-    in_degree = 0
-    out_degree = 0
-    # Loop over each neighbor of the node
-    for neighbor in Graph1.neighbors(node):
-        # If the neighbor has an edge pointing to the node, increment the in-degree of the node
-        if Graph1.has_edge(neighbor, node):
-            in_degree += 1
-        # If the node has an edge pointing to the neighbor, increment the out-degree of the node
-        if Graph1.has_edge(node, neighbor):
-            out_degree += 1
-    # Store the in-degree and out-degree of the node in the degree_dict
-    degree_dict[node] = {'in_degree': in_degree, 'out_degree': out_degree}
-
-# Print the in-degree and out-degree of each node
-for node, degree in degree_dict.items():
-    print(f"Node {node}: In-degree={degree['in_degree']}, Out-degree={degree['out_degree']}")
-    
-frequency_between_two_node={}
-for node, neighbors in neighbors_list.items():
+degree_between_neighbors = {}
+for node, neighbors in Graph1.adjacency():
     for neighbor in neighbors:
-        if Graph1.has_edge(node, neighbor):
-            in_degree = Graph1.in_degree(neighbor)
-            out_degree = Graph1.out_degree(neighbor)
-            
-            frequency_between_two_node[(node, neighbor)] = [in_degree+out_degree]
 
-print("Frequency between 2 node:")
-# print(frequency_between_two_node)
+        # get the degree of the node and neighbor
+        # node_degree = Graph1.degree(node)
+        # neighbor_degree = Graph1.degree(neighbor)
+
+        # get the in-degree and out-degree of the neighbor
+        in_degree = matrix[node][neighbor]
+        out_degree = matrix[neighbor][node]
+
+        # add the node, neighbor, node degree, neighbor degree, in-degree, and out-degree to the dictionary
+        degree_between_neighbors[(node, neighbor)] = [in_degree,out_degree]
+
+# print the degree_between_neighbors dictionary
+print("degree_between_neighbors")
+print(degree_between_neighbors)
 
 
 
@@ -180,22 +139,32 @@ for node, neighbors in neighbors_list.items():
     for neighbor in neighbors:
         if Graph1.has_edge(node, neighbor):
             
-            in_degree = Graph1.in_degree(neighbor)
-            out_degree = Graph1.out_degree(neighbor)
+            in_degree = matrix[node][neighbor]
+            out_degree = matrix[neighbor][node]
             if out_degree == 0:
                 response_score_between_two_node[(node, neighbor)] = [0] # or any other value that makes sense for your use case
             else:
                 response_score_between_two_node[(node, neighbor)] = [in_degree/out_degree]
 
   
+print("response score between 2 node:")
+print(response_score_between_two_node)
 
+frequency_between_two_node={}
+for node, neighbors in neighbors_list.items():
+    for neighbor in neighbors:
+        if Graph1.has_edge(node, neighbor):
+            in_degree = matrix[node][neighbor]
+            out_degree = matrix[neighbor][node]
+            
+            frequency_between_two_node[(node, neighbor)] = [in_degree+out_degree]
 
 print("Frequency between 2 node:")
-# print(response_score_between_two_node)
+print(frequency_between_two_node)
 
 
 
-new_neighbors_list = {}
+Personal_score_list = {}
 for node, neighbors in neighbors_list.items():
     new_neighbors = []
     for neighbor in neighbors:
@@ -205,9 +174,9 @@ for node, neighbors in neighbors_list.items():
             neighbor_social_score = social_scores[neighbor]
             new_value = freq + score + neighbor_social_score
             new_neighbors.append((neighbor, new_value))
-    new_neighbors_list[node] = new_neighbors
+    Personal_score_list[node] = new_neighbors
 print("Personal rank")
-# print(new_neighbors_list)
+print(Personal_score_list)
 
 
 
